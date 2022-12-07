a33cff3e96a2df881bbf82721d70ba145b50f976
James <james@prestwi.ch>
2022-12-07 11:53:55 -0500

    feature: lower bound

--- src/lib.rs
+++ src/lib.rs
@@ -13,8 +13,8 @@ use ethers::{
     providers::{FilterWatcher, Middleware, PendingTransaction, ProviderError},
     signers::Signer,
     types::{
-        transaction::eip2718::TypedTransaction, Address, Bytes, Signature, TransactionReceipt,
-        TxHash, H256, U256,
+        transaction::eip2718::TypedTransaction, Address, Block, Bytes, Signature, Transaction,
+        TransactionReceipt, TxHash, H256, U256, U64,
     },
 };
 use futures::StreamExt;
@@ -27,7 +27,6 @@ type Pbf<'a, M, T> = Pin<Box<dyn Future<Output = Result<T, <M as Middleware>::Er
 ///
 /// As a future, it will resolve to either
 /// - An error from the underlying middleware
-/// - Ok(None) - when another tx has confirmed at the same nonce
 /// - Ok(Some(receipt)) - when this TX has confirmed and the receipt is available
 ///
 /// If the transaction appears to have been dropped from the mempool, this
@@ -35,6 +34,8 @@ type Pbf<'a, M, T> = Pin<Box<dyn Future<Output = Result<T, <M as Middleware>::Er
 /// confirmed, or the account nonce.
 ///
 /// Polling for the nonce increase happens approximately every block
+///
+///
 #[pin_project::pin_project]
 #[must_use = "Futures do nothing unless polled"]
 pub struct TxMan<'a, M>
@@ -146,8 +147,24 @@ where
     InitialBroadcast(Pbf<'a, M, PendingTransaction<'a, M::Provider>>),
     Rebroadcasting(Pbf<'a, M, PendingTransaction<'a, M::Provider>>),
     WaitingForReceipt(Pin<Box<PendingTransaction<'a, M::Provider>>>),
-    GettingNonce(Pbf<'a, M, U256>),
+    GettingNonce {
+        head: Pbf<'a, M, U64>,
+        nonce: Pbf<'a, M, U256>,
+    },
+    Seeking {
+        lower_bound: U64,
+        height: U64,
+        block: Pbf<'a, M, Option<Block<Transaction>>>,
+    },
+    FetchingReceipt {
+        tx_hash: TxHash,
+        receipt: Pbf<'a, M, Option<TransactionReceipt>>,
+    },
     Complete,
+    Delaying {
+        delay: Pin<Box<tokio::time::Sleep>>,
+        next_state: Box<Option<Self>>,
+    },
 }
 
 impl<'a, M> Debug for ManStates<'a, M>
@@ -159,8 +176,13 @@ where
             Self::InitialBroadcast(_) => write!(f, "InitialBroadcast"),
             Self::Rebroadcasting(_) => write!(f, "Rebroadcasting"),
             Self::WaitingForReceipt(_) => write!(f, "WaitingForReceipt"),
-            Self::GettingNonce(_) => write!(f, "GettingNonce"),
+            Self::FetchingReceipt { .. } => write!(f, "FetchingReceipt"),
+            Self::GettingNonce { .. } => write!(f, "GettingNonce"),
+            Self::Seeking { .. } => write!(f, "Seeking"),
             Self::Complete => write!(f, "Complete"),
+            Self::Delaying { next_state, .. } => {
+                f.debug_tuple("Delaying").field(next_state).finish()
+            }
         }
     }
 }
@@ -169,7 +191,7 @@ impl<'a, M> std::future::Future for TxMan<'a, M>
 where
     M: Middleware,
 {
-    type Output = Result<Option<TransactionReceipt>, M::Error>;
+    type Output = Result<TransactionReceipt, M::Error>;
 
     fn poll(self: Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> Poll<Self::Output> {
         let this = self.project();
@@ -196,7 +218,7 @@ where
             ManStates::WaitingForReceipt(pending) => {
                 match (this.blocks.poll_next_unpin(cx), pending.as_mut().poll(cx)) {
                     (_, Poll::Ready(res)) => match res {
-                        Ok(Some(receipt)) => Poll::Ready(Ok(Some(receipt))),
+                        Ok(Some(receipt)) => Poll::Ready(Ok(receipt)),
                         // No receipt
                         Ok(None) => {
                             *this.state = ManStates::Rebroadcasting(Box::pin(
@@ -213,9 +235,12 @@ where
                         }
                     },
                     (Poll::Ready(_), _) => {
-                        *this.state = ManStates::GettingNonce(Box::pin(
-                            this.provider.get_transaction_count(*this.sender, None),
-                        ));
+                        *this.state = ManStates::GettingNonce {
+                            head: Box::pin(this.provider.get_block_number()),
+                            nonce: Box::pin(
+                                this.provider.get_transaction_count(*this.sender, None),
+                            ),
+                        };
                         cx.waker().wake_by_ref();
                         Poll::Pending
                     }
@@ -223,26 +248,107 @@ where
                     (Poll::Pending, Poll::Pending) => Poll::Pending,
                 }
             }
-            ManStates::GettingNonce(fut) => match futures::ready!(fut.as_mut().poll(cx)) {
-                Ok(nonce) => {
-                    if nonce >= *this.nonce {
+            // We get the nonce to
+            ManStates::GettingNonce { head, nonce } => {
+                match (head.as_mut().poll(cx), nonce.as_mut().poll(cx)) {
+                    (Poll::Ready(Err(e)), _) | (_, Poll::Ready(Err(e))) => {
                         *this.state = ManStates::Complete;
-                        Poll::Ready(Ok(None))
-                    } else {
-                        *this.state = ManStates::Rebroadcasting(Box::pin(
-                            this.provider
-                                .send_raw_transaction(this.serialized_tx.clone()),
-                        ));
-                        cx.waker().wake_by_ref();
-                        Poll::Pending
+                        Poll::Ready(Err(e))
                     }
+
+                    (Poll::Ready(Ok(height)), Poll::Ready(Ok(nonce))) => {
+                        if nonce >= *this.nonce {
+                            *this.state = ManStates::Seeking {
+                                lower_bound: height - 15,
+                                height,
+                                block: this.provider.get_block_with_txs(height),
+                            };
+                            cx.waker().wake_by_ref();
+                            Poll::Pending
+                        } else {
+                            *this.state = ManStates::Rebroadcasting(Box::pin(
+                                this.provider
+                                    .send_raw_transaction(this.serialized_tx.clone()),
+                            ));
+                            cx.waker().wake_by_ref();
+                            Poll::Pending
+                        }
+                    }
+                    _ => Poll::Pending,
                 }
-                Err(e) => {
+            }
+            ManStates::Seeking {
+                lower_bound,
+                height,
+                block,
+            } => match block.as_mut().poll(cx) {
+                Poll::Ready(Err(e)) => {
                     *this.state = ManStates::Complete;
                     Poll::Ready(Err(e))
                 }
+                Poll::Ready(Ok(None)) => {
+                    *this.state = ManStates::Delaying {
+                        delay: Box::pin(tokio::time::sleep(Duration::from_secs(2))),
+                        next_state: Box::new(Some(ManStates::GettingNonce {
+                            head: Box::pin(this.provider.get_block_number()),
+                            nonce: Box::pin(
+                                this.provider.get_transaction_count(*this.sender, None),
+                            ),
+                        })),
+                    };
+                    cx.waker().wake_by_ref();
+                    Poll::Pending
+                }
+                Poll::Ready(Ok(Some(block))) => {
+                    if let Some(tx) = block.transactions.iter().find(|tx| tx.nonce == *this.nonce) {
+                        *this.state = ManStates::FetchingReceipt {
+                            tx_hash: tx.hash,
+                            receipt: this.provider.get_transaction_receipt(tx.hash),
+                        };
+                        cx.waker().wake_by_ref();
+                        Poll::Pending
+                    } else {
+                        let height = *height - 1;
+                        *this.state = ManStates::Seeking {
+                            lower_bound: *lower_bound,
+                            height,
+                            block: Box::pin(this.provider.get_block_with_txs(height)),
+                        };
+                        Poll::Pending
+                    }
+                }
+                Poll::Pending => Poll::Pending,
             },
+            ManStates::FetchingReceipt { tx_hash, receipt } => {
+                match futures::ready!(receipt.as_mut().poll(cx)) {
+                    Ok(Some(receipt)) => {
+                        *this.state = ManStates::Complete;
+                        Poll::Ready(Ok(receipt))
+                    }
+                    Ok(None) => {
+                        *this.state = ManStates::Delaying {
+                            delay: Box::pin(tokio::time::sleep(Duration::from_secs(2))),
+                            next_state: Box::new(Some(ManStates::FetchingReceipt {
+                                tx_hash: *tx_hash,
+                                receipt: this.provider.get_transaction_receipt(*tx_hash),
+                            })),
+                        };
+                        cx.waker().wake_by_ref();
+                        Poll::Pending
+                    }
+                    Err(e) => {
+                        *this.state = ManStates::Complete;
+                        Poll::Ready(Err(e))
+                    }
+                }
+            }
             ManStates::Complete => panic!("polled after completion"),
+            ManStates::Delaying { delay, next_state } => {
+                futures::ready!(delay.as_mut().poll(cx));
+                *this.state = next_state.take().expect("implementation error");
+                cx.waker().wake_by_ref();
+                Poll::Pending
+            }
         }
     }
 }
